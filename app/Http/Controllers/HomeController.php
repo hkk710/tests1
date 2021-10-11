4a9ac74be3799378e230f0b8a764da1763ec54d7
Advaith A J <advaitharunjeena@gmail.com>
2021-10-11 23:11:42 +0530

    Controller logic updated

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,17 +13,16 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-         $this->middleware([ 'auth.admin' ]);
+        $this->middleware('guard.auth');
     }
 
     /**
      * Show the application dashboard.
      *
-     * @return \Illuminate\Contracts\Support\Renderable
+     * @return \Illuminate\Http\Response
      */
-    public function home()
+    public function index()
     {
-        return view('home');
+        return view('pages.home');
     }
-
 }
